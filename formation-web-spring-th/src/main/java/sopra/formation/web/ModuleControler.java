2a1a9d1520a08c8a314bd5343e998786457b9439
Baptiste <baptiste.devaux.1993@gmail.com>
2019-11-25 14:13:36 +0100

    module (-test, -liens)

---
+++ formation-web-spring-th/src/main/java/sopra/formation/web/ModuleControler.java
@@ -0,0 +1,120 @@
+package sopra.formation.web;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import sopra.formation.model.Module;
+import sopra.formation.repository.IModuleRepository;
+import sopra.formation.validator.ModuleValidator;
+
+@Controller
+@RequestMapping("/module")
+public class ModuleControler {
+
+	@Autowired
+	private IModuleRepository moduleRepo;
+
+	public ModuleControler() {
+		super();
+	}
+
+	@GetMapping({ "", "/list" })
+	public String list(Model model) {
+		List<Module> modules = moduleRepo.findAll();
+
+		model.addAttribute("modules", modules);
+		return "module/list";
+
+	}
+
+	@GetMapping("/add")
+	public String add(Model model) {
+//		model.addAttribute("Filiere", new Filiere());
+//		model.addAttribute("Salle", new Salle());
+//		model.addAttribute("Formateur", new Formateur());
+//		model.addAttribute("Matiere", new Matiere()); faire le findAllOrphan()
+
+		model.addAttribute("Module", new Module());
+
+		return "module/form";
+
+	}
+
+	@GetMapping("/edit")
+	public String edit(@RequestParam("code") Integer code, Model model) {
+
+		model.addAttribute("Module", moduleRepo.findById(code).get());
+
+//		model.addAttribute("Filiere", filiereRepo.findAllOrphanAndCurrentStagiaire(id)); faire la méthode dans le repo
+//		model.addAttribute("Salle", salleRepo.findAllOrphanAndCurrentStagiaire(id)); faire la méthode dans le repo
+//		model.addAttribute("Formateur", formateurRepo.findAllOrphanAndCurrentStagiaire(id)); faire la méthode dans le repo
+//		model.addAttribute("Matiere", matiereRepo.findAllOrphanAndCurrentStagiaire(id)); faire la méthode dans le repo
+		return "module/form";
+	}
+
+	@PostMapping("/save")
+	public String save(@ModelAttribute("module") @Valid Module module, BindingResult result, Model model) {
+		new ModuleValidator().validate(module, result);
+
+//		if (result.hasErrors()) {
+
+//			if (module.getCode() != null) {
+//				model.addAttribute("Filiere", filiereRepo.findAllOrphanAndCurrentModule(module.getCode()));
+//				model.addAttribute("Salle", salleRepo.findAllOrphanAndCurrentModule(module.getCode()));
+//				model.addAttribute("Formateur", formateurRepo.findAllOrphanAndCurrentModule(module.getCode()));
+//				model.addAttribute("Matiere", matiereRepo.findAllOrphanAndCurrentModule(module.getCode()));
+//			} else {
+//				model.addAttribute("Filiere", filiereRepo.findAllOrphan());
+//				model.addAttribute("Salle", salleRepo.findAllOrphan());
+//				model.addAttribute("Formateur", formateurRepo.findAllOrphan());
+//				model.addAttribute("Matiere", matiereRepo.findAllOrphan());
+//			}
+
+//			return "module/form";
+//		}
+
+		if (module.getFiliere().getId() == null)
+
+		{
+			module.setFiliere(null);
+		}
+		if (module.getSalle().getNom() == null)
+
+		{
+			module.setSalle(null);
+		}
+		if (module.getFormateur().getId() == null)
+
+		{
+			module.setFormateur(null);
+		}
+		if (module.getMatiere().getId() == null)
+
+		{
+			module.setMatiere(null);
+		}
+
+		moduleRepo.save(module);
+
+		return "redirect:/stagiaire/list";
+	}
+
+	public String cancel(Model model) {
+		return "forward:/module/list";
+	}
+
+	public String delete(Model model) {
+		return list(model);
+	};
+}
