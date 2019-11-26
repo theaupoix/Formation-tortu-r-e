9b5bc03a1f6fec0f9b24c71a57b35440acd1f0fc
Baptiste <baptiste.devaux.1993@gmail.com>
2019-11-26 09:31:27 +0100

    fin Module -liens (manques de données)

--- formation-web-spring-th/src/main/java/sopra/formation/validator/ModuleValidator.java
+++ formation-web-spring-th/src/main/java/sopra/formation/validator/ModuleValidator.java
@@ -3,18 +3,32 @@ package sopra.formation.validator;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 
-public class ModuleValidator implements Validator{
+import sopra.formation.model.Module;
+
+public class ModuleValidator implements Validator {
 
 	@Override
 	public boolean supports(Class<?> clazz) {
-		// TODO Auto-generated method stub
-		return false;
+		return Module.class.equals(clazz);
 	}
 
 	@Override
 	public void validate(Object target, Errors errors) {
-		// TODO Auto-generated method stub
+		Module module = (Module) target;
 		
+		if (module.getMatiere() == null || module.getMatiere().getId() == null) {
+			errors.rejectValue("matiere.id", "notnull", "Matiere obligatoire");
+		}
+		if (module.getFiliere() == null || module.getFiliere().getId() == null) {
+			errors.rejectValue("filiere.id", "notnull", "Filiere obligatoire");
+		}
+		if (module.getFormateur() == null || module.getFormateur().getId() == null) {
+			errors.rejectValue("formateur.id", "notnull", "Formateur obligatoire");
+		}
+		if (module.getSalle() == null || module.getSalle().getNom() == null) {
+			errors.rejectValue("salle.nom", "notnull", "Salle obligatoire");
+		}
+
 	}
 
 }
